5e6460a90c5d03dddd1c9c22d3b4aa42af58108f
BeAsT436 <yuriryabenko436@gmail.com>
2024-10-18 10:45:45 +0300

    add user registration

---
+++ src/middlewares/validate-request.ts
@@ -0,0 +1,11 @@
+import {ObjectSchema} from "joi"
+import { Request, Response, NextFunction } from "express"
+export const validateRequests = (schema: ObjectSchema)=>{return(req:Request, res:Response, next:NextFunction)=>{
+    const {error} = schema.validate(req.body)
+    if (error) {
+        console.log(error.details[0].message);
+        return res.status(400).json({error: error.details[0].message})
+        
+    }
+    next()
+}}
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import {createUser, getAllUsers} from "../controllers/user-controller"
+import { validateRequests } from "../middlewares/validate-request";
+import { createUserSchema } from "../dtos/UserDTO";
 
 const router = Router()
 
 router.get("/", getAllUsers)
-router.post("/", createUser)
+router.post("/", validateRequests(createUserSchema), createUser)
 
 
 export default router
